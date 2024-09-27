a12b1ca60dbab0f6f24b43babecf9d62219e2a82
Roberto Díaz <rdiazcastro@me.com>
2024-09-27 17:15:52 -0600

    Se corrige HTML y JS y se adjuntan cambios

--- filtro.js
+++ filtro.js
@@ -8,54 +8,42 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const listaProductos = document.getElementById("lista-de-productos") // Se corrige getElementsbyName a byId y se cambia nombre de variable
+const inputBusqueda = document.querySelector("input"); // Se elimina el punto de .input y se cambia nombre de variable
+const botonDeFiltro = document.querySelector("button"); // Se mueve de lugar esta variable
 
+// Se crea función para mostrar los productos
+function displayProductos(productos) {
+  listaProductos.innerHTML = '';
+
+// Se cambia d por div
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  const div = document.createElement("div");
+  div.classList.add("producto");
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  // Se cambia ti por titulo
+  const titulo = document.createElement("p");
+  titulo.classList.add("titulo");
+  titulo.textContent = productos[i].nombre;
   
-  var imagen = document.createElement("img");
+  const imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
+  div.appendChild(titulo);
+  div.appendChild(imagen);
+  listaProductos.appendChild(div);
+  }
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+displayProductos(productos);
 
 botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
-  }
+  //Se elimina for para ser reemplazado por la función displayProductos, misma que se reutiliza
 
-  const texto = $i.value;
-  console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
+  // Se cambia nombre de variable a textoBusqueda
+  const textoBusqueda = inputBusqueda.value;
+  const productosFiltrados = filtrado(productos, textoBusqueda );
+  displayProductos(productosFiltrados);
 }
 
 const filtrado = (productos = [], texto) => {
